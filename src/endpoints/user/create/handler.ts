fea545b6446e6953885e7886e05c8829394d2b80
kporadnik <kporadnik3@gmail.com>
2025-06-25 12:44:47 +0200

    feat: aws sam configuration

--- src/endpoints/user/create/handler.ts
+++ src/endpoints/user/create/handler.ts
@@ -1,7 +1,19 @@
 import { CreateLambdaResponse } from "@/utils";
 import { APIGatewayEvent, Context } from "aws-lambda";
 
-export function handler(event: APIGatewayEvent, ctx: Context) {
+const environment = {
+  USERS_TABLE_NAME: process.env.USERS_TABLE_NAME,
+};
+
+export async function handler(event: APIGatewayEvent, ctx: Context) {
+  const { USERS_TABLE_NAME } = environment;
+
+  console.log({
+    environment,
+    method: event.httpMethod,
+    body: event.body,
+  });
+
   return CreateLambdaResponse(200, {
     message: "Create",
   });
--- src/endpoints/user/update/handler.ts
+++ src/endpoints/user/update/handler.ts
@@ -1,7 +1,18 @@
 import { CreateLambdaResponse } from "@/utils";
 import { APIGatewayEvent, Context } from "aws-lambda";
 
-export function handler(event: APIGatewayEvent, ctx: Context) {
+const environment = {
+  USERS_TABLE_NAME: process.env.USERS_TABLE_NAME,
+};
+
+export async function handler(event: APIGatewayEvent, ctx: Context) {
+  console.log({
+    environment,
+    method: event.httpMethod,
+    body: event.body,
+    userId: event.pathParameters?.userId,
+  });
+
   return CreateLambdaResponse(200, {
     message: "Update",
   });
