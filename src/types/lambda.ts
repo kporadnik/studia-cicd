1d4e1e6eb01d55620b4b66f2830caf7d280663f5
kporadnik <kporadnik3@gmail.com>
2025-06-25 14:20:58 +0200

    fix: change type for json parse middleware

--- src/types/lambda.ts
+++ src/types/lambda.ts
@@ -1,4 +1,6 @@
 import { APIGatewayEvent, Context } from "aws-lambda";
 
-export type TLambdaEvent = APIGatewayEvent;
+export type TLambdaEvent = Omit<APIGatewayEvent, "body"> & {
+  body: Record<string, any>;
+};
 export type TLambdaContext = Context;
